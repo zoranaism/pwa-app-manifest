a9de73c6d94f36f853cd3eff4de5917f441f054f
zoranaism <lazareviczorana@gmail.com>
2020-08-21 12:47:22 +0200

    Service worker

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,3 +1,5 @@
+var deferredPrompt;
+
 if ("serviceWorker" in navigator) {
   // navigator is the browser
   navigator.serviceWorker
@@ -6,3 +8,10 @@ if ("serviceWorker" in navigator) {
         console.log('Service worker registered!');
      });
 }
+
+window.addEventListener("beforeinstallprompt", function(event) {
+  console.log("before install prompt fired");
+  event.preventDefault(); 
+  deferredPrompt = event;
+  return false;
+})
